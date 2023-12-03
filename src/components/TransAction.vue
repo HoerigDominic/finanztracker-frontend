e38acc44fb4b0f726a5ac1a14852c47892b6d05f
HoerigDominic <s0578252@htw-berlin.de>
2023-12-03 21:44:44 +0100

    Verbindung Back -und Frontend erstellt

---
+++ src/components/TransAction.vue
@@ -0,0 +1,86 @@
+<template>
+    <h1>Finanztracker</h1>
+    <div class="transaction">
+        <table class="centered-table">
+            <thead>
+            <tr>
+                <th>Datum</th>
+                <th>Art</th>
+                <th>Betrag</th>
+                <th>Kategorie</th>
+                <th>Beschreibung</th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="transaction in transactions" :key="transaction.id">
+                <td>{{ transaction.datum }}</td>
+                <td>{{ transaction.art }}</td>
+                <td>{{ transaction.betrag}}</td>
+                <td>{{ transaction.kategorie }}</td>
+                <td>{{ transaction.beschreibung }}</td>
+                <td>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+    data() {
+        return {
+            transactions: [],
+            datum: '',
+            art: '',
+            betrag: '',
+            kategorie: '',
+            beschreibung: '',
+            aktionen:''
+        };
+    },
+    methods: {
+
+    },
+
+    mounted() {
+        const requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        }
+
+        fetch('http://localhost:8080/transaktionen',requestOptions)
+            .then(response => response.json())
+            .then(result => result.forEach(transaction =>{
+                this.transactions.push(transaction)
+            }))
+            .catch(error => console.log('error',error))
+    }
+
+};
+</script>
+
+<style>
+.transaction {
+    display: flex;
+    justify-content: center;
+}
+
+.centered-table {
+    /* Stile für die Tabelle */
+    border-collapse: collapse;
+    width: 70%; /* Beispiel für die Breite der Tabelle */
+    margin-top: 20px; /* Beispiel für den oberen Abstand */
+}
+.centered-table th, .centered-table td {
+    border: 1px solid #ddd;
+    padding: 8px;
+    text-align: left;
+}
+.centered-table th {
+    background-color: #f2f2f2;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Hello World"/>
+    <TransAction/>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import HelloWorldTwo from "@/components/TransAction.vue";
+import TransAction from "@/components/TransAction.vue";
+import OffCanvas from "@/components/OffCanvas.vue";
 
 @Options({
   components: {
-    HelloWorld,
+      OffCanvas,
+      TransAction
   },
 })
 export default class HomeView extends Vue {}
