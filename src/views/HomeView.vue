be678ead19814ba8e91ca98a442fcf6e6f084f0a
HoerigDominic <s0578252@htw-berlin.de>
2024-01-14 16:33:43 +0100

    Import componente gelöscht

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,6 @@
 </template>
 
 <script lang="ts">
-//import TransactionCreate from "@/components/TransactionCreate.vue";
 import { Options, Vue } from 'vue-class-component';
 import NavBar from "@/components/Navbar.vue";
 import TransactionComp from "@/components/TransactionsComp.vue";
